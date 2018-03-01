82309cdf5ffc5f1332c55f44cd9ae1e06c2e4b07
Mikael Lindemann <mikael@mlindemann.dk>
2018-03-01 19:01:42 +0100

    Implemented ToString() for result class

--- Qualification/Qualification/QualificationResult.cs
+++ Qualification/Qualification/QualificationResult.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Windemann.HashCode.Qualification
 {
     public class QualificationResult
     {
+        public List<List<int>> Assignments { get; }
+
+        public QualificationResult()
+        {
+            Assignments = new List<List<int>>();
+        }
+
+        public QualificationResult(List<List<int>> assignments)
+        {
+            Assignments = assignments;
+        }
+
+        public void AddAssignment(List<int> rideIds)
+        {
+            Assignments.Add(rideIds);
+        }
+        
         public override string ToString()
         {
-            throw new NotImplementedException();
+            var builder = new StringBuilder();
+            
+            foreach (var assignment in Assignments)
+            {
+                builder.Append(assignment.Count);
+
+                foreach (var rideId in assignment)
+                {
+                    builder.Append(" ").Append(rideId);
+                }
+
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
         }
     }
 }
