092904cae42e84824953d2a035cd687eb08b9455
Anders Wind <awia00@gmail.com>
2018-03-01 18:41:59 +0100

    Added ride model

---
+++ Qualification/Qualification/Model/Ride.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windemann.HashCode.Qualification.Model
+{
+    class Ride
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+}
