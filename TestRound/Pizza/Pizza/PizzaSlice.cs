ea553cdee127a85b64aab5fc29a144da6903ce84
Mikael Lindemann <mikael@mlindemann.dk>
2018-03-01 17:11:11 +0100

    Implementation of test round

--- TestRound/Pizza/Pizza/PizzaSlice.cs
+++ TestRound/Pizza/Pizza/PizzaSlice.cs
@@ -1,23 +1,37 @@
-﻿namespace Pizza
+﻿using System;
+using System.Threading;
+
+namespace Pizza
 {
     public class PizzaSlice
     {
-        public int TopRow { get; set; }
-        public int LeftColumn { get; set; }
+        public int Size { get; }
         
-        public int BottomRow { get; set; }
-        public int RightColumn { get; set; }
+        public int TopRow { get; }
+        public int LeftColumn { get; }
+        
+        public int BottomRow { get; }
+        public int RightColumn { get; }
+
+        public PizzaSlice(int topRow, int leftColumn, int bottomRow, int rightColumn)
+        {
+            TopRow = topRow;
+            LeftColumn = leftColumn;
+            BottomRow = bottomRow;
+            RightColumn = rightColumn;
+            Size = (bottomRow - topRow + 1) * (rightColumn - leftColumn + 1);
+        }
 
         public bool IsValid(PizzaInstance instance)
         {
             if (TopRow > BottomRow) return false;
             if (LeftColumn > RightColumn) return false;
-            if ((BottomRow - TopRow) * (LeftColumn - RightColumn) > instance.MaximumCellsInSlice) return false;
+            if (Size > instance.MaximumCellsInSlice) return false;
 
             int m = 0, t = 0;
-            for (var row = TopRow; row < BottomRow; row++)
+            for (var row = TopRow; row <= BottomRow; row++)
             {
-                for (var col = LeftColumn; col < RightColumn; col++)
+                for (var col = LeftColumn; col <= RightColumn; col++)
                 {
                     if (instance.Pizza[row, col] == PizzaIngredient.Mushroom)
                         m++;
@@ -29,6 +43,12 @@
             if (m < instance.MinimumOfEachIngredient || t < instance.MinimumOfEachIngredient) return false;
 
             return true;
+        }
+
+        public bool Overlaps(PizzaSlice other)
+        {
+            return (LeftColumn < other.RightColumn && RightColumn > other.LeftColumn && BottomRow > other.TopRow && TopRow < other.BottomRow) ||
+                   (other.LeftColumn < LeftColumn && RightColumn < other.RightColumn && other.TopRow < TopRow && BottomRow < other.BottomRow);
         }
 
         public override string ToString()
--- TestRound/Pizza/Pizza/Program.cs
+++ TestRound/Pizza/Pizza/Program.cs
@@ -1,26 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
 
 namespace Pizza
 {
     class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
             var parser = new PizzaParser();
 
-            var instance = parser.ParseInstance("/home/mikael/Hentet/small.in");
-            
-            
-            
-            
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: dotnet Pizza.dll <filename>");
+                return;
+            }
 
-            Console.WriteLine(instance);
+            var instance = parser.ParseInstance(args[0]);
+
+            Console.Error.WriteLine("Generating all slices");
+            
+            var slices = GenerateAllSlices(instance).ToList();
+            
+            Console.Error.WriteLine($"{slices.Count} slices generated");
+            Console.Error.WriteLine("Ordering all slices by size");
+                
+            var orderedSlices = slices.AsParallel().OrderByDescending(s => s.Size);
+            
+            Console.Error.WriteLine("Doing greedy heuristic");
+
+            var solution = new List<PizzaSlice>();
+
+            var bitmap = new bool[instance.Rows, instance.Columns];
+
+            var count = 0;
+            foreach (var slice in orderedSlices)
+            {
+                count++;
+                var isGood = true;
+                for (var row = slice.TopRow; row <= slice.BottomRow; ++row)
+                {
+                    for (var col = slice.LeftColumn; col <= slice.RightColumn; ++col)
+                    {
+                        if (bitmap[row, col])
+                        {
+                            isGood = false;
+                            break;
+                        }
+                    }
+
+                    if (!isGood) break;
+                }
+
+                if (isGood)
+                {
+                    for (var row = slice.TopRow; row <= slice.BottomRow; ++row)
+                    {
+                        for (var col = slice.LeftColumn; col <= slice.RightColumn; ++col)
+                        {
+                            bitmap[row, col] = true;
+                        }
+                    }
+
+                    solution.Add(slice);
+                    Console.Error.WriteLine($"Number of slices taken: {solution.Count} - Slices considered: {count}");
+                }
+            }
+
+            Console.WriteLine(new PizzaResult
+            {
+                Slices = solution
+            });
+            
+            Console.Error.WriteLine(solution.Count);
         }
 
-        private IEnumerable<PizzaSlice> GenerateAllSlices(PizzaInstance instance)
+        private static IEnumerable<PizzaSlice> GenerateAllSlices(PizzaInstance instance)
         {
-            
+            for (var startRow = 0; startRow < instance.Rows; startRow++)
+            {
+                for (var startCol = 0; startCol < instance.Columns; startCol++)
+                {
+                    for (var rows = 1; startRow + rows < instance.Rows && rows < instance.MaximumCellsInSlice; rows++)
+                    {
+                        for (var cols = Math.Max(1, instance.MinimumOfEachIngredient / rows); startCol + cols < instance.Columns && rows * cols < instance.MaximumCellsInSlice; cols++)
+                        {
+                            var slice = new PizzaSlice(startRow, startCol, startRow + rows - 1, startCol + cols - 1);
+
+                            if (slice.IsValid(instance))
+                            {
+                                yield return slice;
+                            }                            
+                        }                        
+                    }
+                }                
+            }
         }
     }
 }
